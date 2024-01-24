8d9c34ff22eac9d1c1a470bd08d22e24ce0da063
Eshan Jain <eshajain@amazon.com>
2024-01-24 14:17:00 -0800

    Initial Commit

---
+++ macOSApp2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  macOSApp2
+//
+//  Created by Jain, Eshan on 1/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ macOSApp2/macOSApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  macOSApp2App.swift
+//  macOSApp2
+//
+//  Created by Jain, Eshan on 1/24/24.
+//
+
+import SwiftUI
+
+@main
+struct macOSApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
